bcfb7d614e61c9d8b685328fdc5b8941bcea52cc
김동민 <kemin1910@gmail.com>
2024-03-17 00:34:14 +0900

    2024-03-17
    1. 메인 메뉴와 게임 모드 선택 판넬을 분리
    2. 게임 모드 선택 버튼 코드 추가
    3. 게임 모드 선택 판넬 활성화 및 뒤로가기 활성화

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -4,44 +4,62 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject mainMenuPanel;
+    public GameObject gameModePanel;
+
     void Start()
     {
-        
+        mainMenuPanel.SetActive(true);
+        gameModePanel.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    // 새 게임 버튼 클릭 함수
     public void OnClickNewGame()
     {
         Debug.Log("새 게임");
+        mainMenuPanel.SetActive(false);
+        gameModePanel.SetActive(true);
     }
 
+    // 백과사전 버튼 클릭 함수
     public void OnClickEncyclopedia()
     {
         Debug.Log("백과사전");
     }
 
+    // 통계 버튼 클릭 함수
     public void OnClickStatistics()
     {
         Debug.Log("통계");
     }
 
+    // 설정 버튼 클릭 함수
     public void OnClickSetting()
     {
         Debug.Log("설정");
     }
 
+    // 종료 버튼 클릭 함수
     public void OnClickExit()
     {
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
+#if UNITY_EDITOR                                         // 에디터에서 플레이할 시
+        UnityEditor.EditorApplication.isPlaying = false; // 에디터 종료
 #else
         Application.Quit();
 #endif
+    }
+
+    // 뒤로가기 버튼 클릭 함수
+    public void OnClickBack()
+    {
+        Debug.Log("뒤로가기");
+        mainMenuPanel.SetActive(true);
+        gameModePanel.SetActive(false);
+    }
+
+    // 일반 게임 버튼 클릭 함수
+    public void OnClickNormal()
+    {
+        Debug.Log("일반 게임");
     }
 }
