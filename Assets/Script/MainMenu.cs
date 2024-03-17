b48ac53217ce79bdfeeeb247407adfdba94aa910
김동민 <kemin1910@gmail.com>
2024-03-18 02:51:13 +0900

    2024-03-18
    1. 캐릭터 선택창 판넬 생성
    2. 아이언 클래드 선택 화면 생성
    3. 캐릭터 선택창에서 뒤로가기 및 출정 기능 구현

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    public GameObject mainMenuPanel;
-    public GameObject gameModePanel;
+    // 판넬
+    public GameObject mainMenuPanel;        // 메인 메뉴
+    public GameObject gameModePanel;        // 게임 모드
+    public GameObject characterSelectPanel; // 캐릭터 선택
+
+    // 캐릭터 배경
+    public GameObject[] characterBackground;
+
+    // 출정 버튼
+    public GameObject embarkButton;
+
+    // 선택 캐릭터 구분 변수 - 0: 아이언클래드
+    private int selectCharacter;
 
     void Start()
     {
         mainMenuPanel.SetActive(true);
         gameModePanel.SetActive(false);
+        characterSelectPanel.SetActive(false);
+        ResetCharacterSelectPanel();
     }
 
     // 새 게임 버튼 클릭 함수
@@ -49,8 +62,8 @@ public class MainMenu : MonoBehaviour
 #endif
     }
 
-    // 뒤로가기 버튼 클릭 함수
-    public void OnClickBack()
+    // 게임 모드 선택에서 뒤로가기 버튼 클릭 함수
+    public void OnClickModeBack()
     {
         Debug.Log("뒤로가기");
         mainMenuPanel.SetActive(true);
@@ -61,5 +74,68 @@ public class MainMenu : MonoBehaviour
     public void OnClickNormal()
     {
         Debug.Log("일반 게임");
+        gameModePanel.SetActive(false);
+        characterSelectPanel.SetActive(true);
+        ResetCharacterSelectPanel();
+    }
+
+    // 캐릭터 선택 화면 초기화
+    private void ResetCharacterSelectPanel()
+    {
+        // 선택 캐릭터 없음
+        selectCharacter = -1;
+
+        // 모든 캐릭터 배경 off
+        for (int i = 0; i < characterBackground.Length; i++)
+        {
+            characterBackground[i].SetActive(false);
+        }
+
+        // 출정 버튼 비활성화
+        embarkButton.SetActive(false);
+    }
+
+    // 캐릭터 선택에서 뒤로가기 버튼 클릭 함수
+    public void OnClickSelectBack()
+    {
+        Debug.Log("뒤로가기");
+        gameModePanel.SetActive(true);
+        characterSelectPanel.SetActive(false);
+    }
+
+    // 아이언 클래드 선택 버튼 클릭 함수
+    public void OnClickSelectIronClad()
+    {
+        Debug.Log("아이언클래드 선택");
+
+        // 아이언클래드 0번
+        selectCharacter = 0;
+
+        // 아이언 클래드 외의 캐릭터 배경 off
+        for (int i = 0; i < characterBackground.Length; i++)
+        {
+            if (i == selectCharacter)
+            {
+                characterBackground[i].SetActive(true);
+            }
+            else
+            {
+                characterBackground[i].SetActive(false);
+            }
+        }
+
+        // 출정 버튼 활성화
+        embarkButton.SetActive(true);
+    }
+
+    // 출정 버튼 클릭 함수
+    public void OnClickEmbark()
+    {
+        Debug.Log("출정");
+        // 아이언클래드 0번
+        if(selectCharacter == 0)
+        {
+            Debug.Log("아이언클래드로 출정");
+        }
     }
 }
