51d90c58f296c9120551314912abd8fa8c2c9be6
김동민 <kemin1910@gmail.com>
2024-03-25 16:09:04 +0900

    Map 생성 알고리즘 테스트

--- Assets/keminTest/Script/MapGenerator.cs
+++ Assets/keminTest/Script/MapGenerator.cs
@@ -5,9 +5,10 @@ using UnityEngine.UI;
 
 public class MApGenerator : MonoBehaviour
 {
-    public GameObject buttonPrefab;
     public Transform gridPanel;
-    private GameObject[,] grid = new GameObject[7, 15];
+    private Vector2[,] gridPositions = new Vector2[7, 15];
+    private List<List<Vector2>> paths = new List<List<Vector2>>();
+    private List<Color> pathColors = new List<Color>();
 
     // Start is called before the first frame update
     void Start()
@@ -18,40 +19,53 @@ public class MApGenerator : MonoBehaviour
 
     void CreateGrid()
     {
-        for(int x = 0; x < 7; x++)
+        for (int x = 0; x < 7; x++)
         {
-            for(int y = 0; y < 15; y++)
+            for (int y = 0; y < 15; y++)
             {
-                GameObject buttonObj = Instantiate(buttonPrefab, gridPanel);
-                buttonObj.name = $"Button_{x}_{y}";
-                buttonObj.transform.localPosition = new Vector2(x * 100, y * -100);
-                grid[x, y] = buttonObj;
+                Vector2 position = new Vector2(x * 200 + 400, y * 150 + 100);
+                gridPositions[x, y] = position;
             }
         }
     }
 
     void GeneratePaths()
     {
-        for(int i = 0; i < 6; i++)
+        for (int pathNum = 0; pathNum < 6; pathNum++)
         {
-            int currentX = Random.Range(0, 7);
-            for(int y = 0; y < 15; y++)
+            int currentX = Random.Range(0, 7); // 시작 x 위치 랜덤 선택
+            List<Vector2> path = new List<Vector2>(); // 이 경로의 위치를 저장할 리스트
+
+            for (int y = 0; y < 15; y++) // 모든 층을 탐색
             {
-                Image buttonImage = grid[currentX, y].GetComponent<Image>();
-                if (buttonImage != null)
-                {
-                    buttonImage.color = Random.ColorHSV();
-                }
-                else
-                {
-                    Debug.LogError("No Image component found on the button prefab!");
-                }
+                path.Add(gridPositions[currentX, y]); // 현재 위치를 경로에 추가
+
+                // 다음 층의 x 위치를 결정
                 List<int> possibleX = new List<int>();
                 if (currentX > 0) possibleX.Add(currentX - 1);
-                if (currentX < 6) possibleX.Add(currentX + 1);
                 possibleX.Add(currentX);
+                if (currentX < 6) possibleX.Add(currentX + 1);
 
-                currentX = possibleX[Random.Range(0, possibleX.Count)];
+                currentX = possibleX[Random.Range(0, possibleX.Count)]; // 다음 x 위치 랜덤 선택
+            }
+
+            paths.Add(path); // 생성된 경로를 경로 리스트에 추가
+            pathColors.Add(Random.ColorHSV());
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (paths.Count > 0)
+        {
+            for(int pathIndex = 0; pathIndex < paths.Count; pathIndex++)
+            {
+                var path = paths[pathIndex];
+                Gizmos.color = pathColors[pathIndex];
+                for(int i = 0; i < path.Count - 1; i++)
+                {
+                    Gizmos.DrawLine(path[i], path[i + 1]);
+                }
             }
         }
     }
