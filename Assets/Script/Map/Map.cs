f918cef20cc0a81a908ed4f2c937a6722c56ea35
YEAHWA1010 <109790117+YEAHWA1010@users.noreply.github.com>
2024-04-01 19:03:25 +0900

    0401#1
    이미지 추가

--- Assets/Script/Map/Map.cs
+++ Assets/Script/Map/Map.cs
@@ -1,121 +1,155 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Tilemaps;
+using UnityEngine.UI;
 
 public class Map : MonoBehaviour
 {
-    [SerializeField] Vector2Int mapSize; //원하는 맵의 크기
-    [SerializeField] float minimumDevideRate; //공간이 나눠지는 최소 비율
-    [SerializeField] float maximumDivideRate; //공간이 나눠지는 최대 비율
-    [SerializeField] private GameObject line; //lineRenderer를 사용해서 공간이 나눠진걸 시작적으로 보여주기 위함
-    [SerializeField] private GameObject map; //lineRenderer를 사용해서 첫 맵의 사이즈를 보여주기 위함
-    [SerializeField] private GameObject roomLine; //lineRenderer를 사용해서 방의 사이즈를 보여주기 위함
-    [SerializeField] private int maximumDepth; //트리의 높이, 높을 수록 방을 더 자세히 나누게 됨
+    // 버튼 프리팹
+    public GameObject buttonPrefab;
+
+    public Transform gridPanel;
+    private Vector2[,] gridPositions = new Vector2[7, 15];
+    private List<List<Vector2>> paths = new List<List<Vector2>>(); //2차원 리스트
+
+    // 경로와 각 방에 해당하는 노드 연결 변수
+    List<int>[] posX = new List<int>[15];
+    HashSet<int>[] usedPosX = new HashSet<int>[15]; //중복 값 허용 x
+
+    // 기즈모 디버깅을 위한 변수
+    HashSet<Color> usedColors = new HashSet<Color>();
+    private List<Color> pathColors = new List<Color>();
+
+    // Start is called before the first frame update
     void Start()
     {
-        Node root = new Node(new RectInt(0, 0, mapSize.x, mapSize.y)); //전체 맵 크기의 루트노드를 만듬
-        DrawMap(0, 0);
-        Divide(root, 0);
-        GenerateRoom(root, 0);
-        GenerateLoad(root, 0);
+        
+
+        CreateGrid();
+        GeneratePaths();
+       
     }
-    private void DrawMap(int x, int y) //x y는 화면의 중앙위치를 뜻함
+
+    void CreateGrid()
     {
-        //기본적으로 mapSize/2라는 값을 계속해서 빼게 될건데, 화면의 중앙에서 화면의 크기의 반을 빼줘야 좌측 하단좌표를 구할 수 있기 때문이다.
-        LineRenderer lineRenderer = Instantiate(map).GetComponent<LineRenderer>();
-        lineRenderer.SetPosition(0, new Vector2(x, y) - mapSize / 2); //좌측 하단
-        lineRenderer.SetPosition(1, new Vector2(x + mapSize.x, y) - mapSize / 2); //우측 하단
-        lineRenderer.SetPosition(2, new Vector2(x + mapSize.x, y + mapSize.y) - mapSize / 2);//우측 상단
-        lineRenderer.SetPosition(3, new Vector2(x, y + mapSize.y) - mapSize / 2); //좌측 상단
-
-    }
-    void Divide(Node tree, int n)
-    {
-        if (n == maximumDepth) return; //내가 원하는 높이에 도달하면 더 나눠주지 않는다.
-
-        //그 외의 경우에는
-
-        int maxLength = Mathf.Max(tree.nodeRect.width, tree.nodeRect.height);
-        //가로와 세로중 더 긴것을 구한후, 가로가 길다면 위 좌, 우로 세로가 더 길다면 위, 아래로 나눠주게 될 것이다.
-        int split = Mathf.RoundToInt(Random.Range(maxLength * minimumDevideRate, maxLength * maximumDivideRate));
-        //나올 수 있는 최대 길이와 최소 길이중에서 랜덤으로 한 값을 선택
-        if (tree.nodeRect.width >= tree.nodeRect.height) //가로가 더 길었던 경우에는 좌 우로 나누게 될 것이며, 이 경우에는 세로 길이는 변하지 않는다.
+        for (int x = 0; x < 7; x++)
         {
-            tree.leftNode = new Node(new RectInt(tree.nodeRect.x, tree.nodeRect.y, split, tree.nodeRect.height));
-            //왼쪽 노드에 대한 정보다 
-            //위치는 좌측 하단 기준이므로 변하지 않으며, 가로 길이는 위에서 구한 랜덤값을 넣어준다.
-            tree.rightNode = new Node(new RectInt(tree.nodeRect.x + split, tree.nodeRect.y, tree.nodeRect.width - split, tree.nodeRect.height));
-            //우측 노드에 대한 정보다 
-            //위치는 좌측 하단에서 오른쪽으로 가로 길이만큼 이동한 위치이며, 가로 길이는 기존 가로길이에서 새로 구한 가로값을 뺀 나머지 부분이 된다.
-            // DrawLine(new Vector2(tree.nodeRect.x + split, tree.nodeRect.y), new Vector2(tree.nodeRect.x + split, tree.nodeRect.y + tree.nodeRect.height));
-            //그 후 위 두개의 노드를 나눠준 선을 그리는 함수이다.        
+            for (int y = 0; y < 15; y++)
+            {
+                Vector2 position = new Vector2(x * 200, y * 150);
+                gridPositions[x, y] = position;
+            }
         }
-        else
-        {
-            tree.leftNode = new Node(new RectInt(tree.nodeRect.x, tree.nodeRect.y, tree.nodeRect.width, split));
-            tree.rightNode = new Node(new RectInt(tree.nodeRect.x, tree.nodeRect.y + split, tree.nodeRect.width, tree.nodeRect.height - split));
-            //  DrawLine(new Vector2(tree.nodeRect.x , tree.nodeRect.y+ split), new Vector2(tree.nodeRect.x + tree.nodeRect.width, tree.nodeRect.y  + split));
-            //세로가 더 길었던 경우이다. 자세한 사항은 가로와 같다.
-        }
-        tree.leftNode.parNode = tree; //자식노드들의 부모노드를 나누기전 노드로 설정
-        tree.rightNode.parNode = tree;
-        Divide(tree.leftNode, n + 1); //왼쪽, 오른쪽 자식 노드들도 나눠준다.
-        Divide(tree.rightNode, n + 1);//왼쪽, 오른쪽 자식 노드들도 나눠준다.
-    }
-    private void DrawLine(Vector2 from, Vector2 to) //from->to로 이어지는 선을 그리게 될 것이다.
-    {
-        LineRenderer lineRenderer = Instantiate(line).GetComponent<LineRenderer>();
-        lineRenderer.SetPosition(0, from - mapSize / 2);
-        lineRenderer.SetPosition(1, to - mapSize / 2);
-    }
-    private RectInt GenerateRoom(Node tree, int n)
-    {
-        RectInt rect;
-        if (n == maximumDepth) //해당 노드가 리프노드라면 방을 만들어 줄 것이다.
-        {
-            rect = tree.nodeRect;
-            int width = Random.Range(rect.width / 2, rect.width - 1);
-            //방의 가로 최소 크기는 노드의 가로길이의 절반, 최대 크기는 가로길이보다 1 작게 설정한 후 그 사이 값중 랜덤한 값을 구해준다.
-            int height = Random.Range(rect.height / 2, rect.height - 1);
-            //높이도 위와 같다.
-            int x = rect.x + Random.Range(1, rect.width - width);
-            //방의 x좌표이다. 만약 0이 된다면 붙어 있는 방과 합쳐지기 때문에,최솟값은 1로 해주고, 최댓값은 기존 노드의 가로에서 방의 가로길이를 빼 준 값이다.
-            int y = rect.y + Random.Range(1, rect.height - height);
-            //y좌표도 위와 같다.
-            rect = new RectInt(x, y, width, height);
-            DrawRectangle(rect);
-        }
-        else
-        {
-            tree.leftNode.roomRect = GenerateRoom(tree.leftNode, n + 1);
-            tree.rightNode.roomRect = GenerateRoom(tree.rightNode, n + 1);
-            rect = tree.leftNode.roomRect;
-        }
-        return rect;
     }
 
-    private void DrawRectangle(RectInt rect)
+    void InitPosXList()
     {
-        LineRenderer lineRenderer = Instantiate(roomLine).GetComponent<LineRenderer>();
-        lineRenderer.SetPosition(0, new Vector2(rect.x, rect.y) - mapSize / 2); //좌측 하단
-        lineRenderer.SetPosition(1, new Vector2(rect.x + rect.width, rect.y) - mapSize / 2); //우측 하단
-        lineRenderer.SetPosition(2, new Vector2(rect.x + rect.width, rect.y + rect.height) - mapSize / 2);//우측 상단
-        lineRenderer.SetPosition(3, new Vector2(rect.x, rect.y + rect.height) - mapSize / 2); //좌측 상단
+        Debug.Log(usedPosX.Length);
+        for (int i = 0; i < 15; i++)
+        {
+            usedPosX[i] = new HashSet<int>();
+            posX[i] = new List<int>();
+        }
     }
-    private void GenerateLoad(Node tree, int n)
-    {
-        if (n == maximumDepth) //리프 노드라면 이을 자식이 없다.
-            return;
-        Vector2Int leftNodeCenter = tree.leftNode.center;
-        Vector2Int rightNodeCenter = tree.rightNode.center;
 
-        DrawLine(new Vector2(leftNodeCenter.x, leftNodeCenter.y), new Vector2(rightNodeCenter.x, leftNodeCenter.y));
-        //세로 기준을 leftnode에 맞춰서 가로 선으로 연결해줌.
-        DrawLine(new Vector2(rightNodeCenter.x, leftNodeCenter.y), new Vector2(rightNodeCenter.x, rightNodeCenter.y));
-        //가로 기준을 rightnode에 맞춰서 세로 선으로 연결해줌.
-        GenerateLoad(tree.leftNode, n + 1); //자식 노드들도 탐색
-        GenerateLoad(tree.rightNode, n + 1);
+    void GeneratePaths()
+    {
+        InitPosXList();
+
+        // 1번째 x값 저장
+        int firstX = 0;
+
+        for (int pathNum = 0; pathNum < 6; pathNum++)
+        {
+            int currentX = Random.Range(0, 7); // 시작 x 위치 랜덤 선택
+
+            // 최소한 2개의 서로 다른 시작 지점 보장
+            if (pathNum == 0) firstX = currentX;
+            else if (pathNum == 1)
+            {
+                while (currentX == firstX)
+                {
+                    currentX = Random.Range(0, 7);
+                }
+            }
+
+            List<Vector2> path = new List<Vector2>(); // 이 경로의 위치를 저장할 리스트
+
+            for (int y = 0; y < 15; y++) // 모든 층을 탐색
+            {
+                path.Add(gridPositions[currentX, y]); // 현재 위치를 경로에 추가
+
+                if (!usedPosX[y].Contains(currentX))
+                {
+                    usedPosX[y].Add(currentX);
+                    posX[y].Add(currentX);
+                }
+
+                // 다음 층의 x 위치를 결정
+                List<int> possibleX = new List<int>();
+                if (currentX > 0) possibleX.Add(currentX - 1);
+                possibleX.Add(currentX);
+                if (currentX < 6) possibleX.Add(currentX + 1);
+
+                currentX = possibleX[Random.Range(0, possibleX.Count)]; // 다음 x 위치 랜덤 선택
+            }
+
+            paths.Add(path); // 생성된 경로를 경로 리스트에 추가
+
+            Color newColor;
+            do
+            {
+                newColor = Random.ColorHSV(0f, 1f, 0.5f, 1f, 0.5f, 1f);
+            } while (usedColors.Contains(newColor));
+            usedColors.Add(newColor);
+            pathColors.Add(newColor);
+        }
+
+        for (int i = 0; i < 15; i++)
+        {
+            for (int j = 0; j < posX[i].Count; j++)
+            {
+                Debug.Log((i) + " " + (posX[i][j]));
+            }
+        }
+
+        CreateButton();
     }
+
+    void CreateButton()
+    {
+        RectTransform panelRectTransform = gridPanel.GetComponent<RectTransform>();
+        float panelWidth = panelRectTransform.rect.width;
+        float panelHeight = panelRectTransform.rect.height;
+
+        for (int i = 0; i < 15; i++)
+        {
+            for (int j = 0; j < posX[i].Count; j++)
+            {
+                // 패널의 중앙에 프리팹을 생성하기 위해 패널의 중앙 좌표를 기준으로 버튼의 위치를 설정합니다.
+                Vector2 buttonPos = new Vector2(panelWidth / 2, panelHeight / 2);
+
+                GameObject buttonObj = Instantiate(buttonPrefab, gridPanel);
+                buttonObj.transform.position = buttonPos;
+            }
+        }
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    if (paths.Count > 0)
+    //    {
+    //        for (int pathIndex = 0; pathIndex < paths.Count; pathIndex++)
+    //        {
+    //            var path = paths[pathIndex];
+    //            Gizmos.color = pathColors[pathIndex];
+    //            for (int i = 0; i < path.Count - 1; i++)
+    //            {
+    //                Gizmos.DrawLine(path[i], path[i + 1]);
+    //            }
+    //        }
+    //    }
+    //}
 }
